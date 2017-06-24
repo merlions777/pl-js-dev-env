b16bd230f604bf87e11036f6698f8e5607c6cdaa
merlions777 <kandakatla.surender@gmail.com>
2017-06-24 16:54:36 -0500

    New Development

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -9,11 +9,20 @@ const port = 2934;
 const app = express();
 const compiler = webpack(config);
 
+/*Bundling - serves files from memory, CSS, imgs, JS, updates code changes.    */
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
+app.get('/users', function(req,res){
+// hard coding , to mock the hit a real database
+res.json([
+  {"id": 1, "firstName":"John", "lastName":"Freeman", "email":"javan@walmart.com"},
+  {"id": 2, "firstName":"Ninja", "lastName":"Ladak", "email":"eladfopr@walmart.com"},
+  {"id": 3, "firstName":"Mayberr", "lastName":"Ray", "email":"dragonw@walmart.com"}
+  ]);
+});
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'../src/index.html'));
