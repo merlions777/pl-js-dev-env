315fc56909afc49e90ce80c72ed8950af22b6910
merlions777 <kandakatla.surender@gmail.com>
2017-07-15 17:54:29 -0500

    Bundle Spliting

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* This file contains reference to the vendor libraires we`re using in this project.
+This is used by webpack */
+
+
+/* eslint-disable no-unused-vars */
+import fetch from 'whatwg-fetch';
